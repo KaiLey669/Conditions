0811c2d0a49eba34e2ad026fd7bc176abdf9c7b0
Vladislav Slobodchikov <vslobodchikov@vvo.local>
2026-06-05 14:15:17 +1000

    Add check vote

---
+++ main.py
@@ -0,0 +1,8 @@
+age = 18
+is_citizen = True
+is_disqualified = False
+
+if age >= 18 and is_citizen and not is_disqualified:
+    print("Разрешено участвовать в голосовании.")
+else:
+    print("Запрещено участие в голосовании")
