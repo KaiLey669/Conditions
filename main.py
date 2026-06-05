38193394416acafe621fe6cbf69b299e2ce175ff
Vladislav Slobodchikov <vslobodchikov@vvo.local>
2026-06-06 00:51:15 +1000

    Change status conditions

--- main.py
+++ main.py
@@ -6,32 +6,22 @@ def get_age_input(fail_message: str):
         return None
 
 def get_citizen_status(fail_message: str):
-    citizen_status = None
-    try:
-        citizen_status = int(input("Являетесь ли вы гражданином страны (1 - Да, 2 - Нет): "))
-        if citizen_status not in [1, 2]:
-            return None
-        elif citizen_status == 1:
-            return True
-        else:
-            return False
-    except ValueError:
-        print(fail_message)
-    return citizen_status
+    citizen_status = input("Являетесь ли вы гражданином страны (Да/Нет): ").lower()
+    if citizen_status == "да":
+        return True
+    elif citizen_status == "нет":
+        return False
+    print(fail_message)
+    return None
 
 def get_disqualified_status(fail_message: str):
-    disqualified_status = None
-    try:
-        disqualified_status = int(input("Были ли вы дисквалифицированы (1 - Да, 2 - Нет): "))
-        if disqualified_status not in [1, 2]:
-            return None
-        elif disqualified_status == 1:
-            return True
-        else:
-            return False
-    except ValueError:
-        print(fail_message)
-    return disqualified_status
+    disqualified_status = input("Были ли вы дисквалифицированы (Да/Нет): ").lower()
+    if disqualified_status == "да":
+        return True
+    elif disqualified_status == "нет":
+        return False
+    print(fail_message)
+    return None
 
 def check_vote_status(fail_message: str):
     age = get_age_input(fail_message)
